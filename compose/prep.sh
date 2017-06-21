30764ddbfeebe67e2610ee9c4eda4953cfdf35cc
naimabdrahman <naim.abdrahman@gmail.com>
2017-06-21 19:59:25 +0800

    Wed Jun 21 19:59:25 +08 2017

---
+++ compose/prep.sh
@@ -0,0 +1,3 @@
+mkdir -p /srv/docker/smartprojects/mysql
+mkdir -p /srv/docker/smartprojects/joomla_www
+mkdir -p /srv/docker/smartprojects/phpmyadmin/sessions
---
+++ compose/unprep.sh
@@ -0,0 +1,3 @@
+rm -rf /srv/docker/smartprojects/mysql
+rm -rf /srv/docker/smartprojects/joomla_www
+rm -rf /srv/docker/smartprojects/phpmyadmin/sessions

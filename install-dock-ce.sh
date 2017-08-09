71eec18d178d9b91c96ef8bde46cf47bd65d5a9a
naimabdrahman <naim.abdrahman@gmail.com>
2017-08-09 21:49:56 +0800

    Create install-dock-ce.sh

---
+++ install-dock-ce.sh
@@ -0,0 +1,10 @@
+yum -y update
+yum remove docker docker-common docker-selinux docker-engine
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum makecache fast
+yum -y install docker-ce
+service docker start
+chkconfig docker on
+yum -y install python-pip
+pip install docker-compose

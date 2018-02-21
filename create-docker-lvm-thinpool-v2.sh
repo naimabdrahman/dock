bb17ad85a4bef691fcee17c7a2cd2db498aa9422
naimabdrahman <naim.abdrahman@gmail.com>
2018-02-21 17:00:17 +0800

    Update create-docker-lvm-thinpool-v2.sh

--- create-docker-lvm-thinpool-v2.sh
+++ create-docker-lvm-thinpool-v2.sh
@@ -1,3 +1,6 @@
+## Create docker lvm thinpool on centos 7 manually
+## /dev/sdb1 should have been fdisked initially
+
 pvcreate /dev/sdb1
 vgcreate docker /dev/sdb1
 lvcreate --wipesignatures y -n thinpool docker -l 95%VG
@@ -5,12 +8,11 @@ lvcreate --wipesignatures y -n thinpoolmeta docker -l 1%VG
 lvconvert -y --zero n -c 512K --thinpool docker/thinpool --poolmetadata docker/thinpoolmeta
 
 
-vi /etc/lvm/profile/docker-thinpool.profile
-
-activation {
-  thin_pool_autoextend_threshold=80
-  thin_pool_autoextend_percent=20
-}
+touch /etc/lvm/profile/docker-thinpool.profile
+echo -e 'activation {' > /etc/lvm/profile/docker-thinpool.profile
+echo -e '  thin_pool_autoextend_threshold=80' >> /etc/lvm/profile/docker-thinpool.profile
+echo -e '  thin_pool_autoextend_percent=20' >> /etc/lvm/profile/docker-thinpool.profile
+echo -e '}' >> /etc/lvm/profile/docker-thinpool.profile
 
 
 lvchange --metadataprofile docker-thinpool docker/thinpool
@@ -20,12 +22,17 @@ service docker stop
 mkdir /var/lib/docker.bk
 mv /var/lib/docker/* /var/lib/docker.bk
 
+
 touch /etc/docker/daemon.json
-{
-    "storage-driver": "devicemapper",
-    "storage-opts": [
-    "dm.thinpooldev=/dev/mapper/docker-thinpool",
-    "dm.use_deferred_removal=true",
-    "dm.use_deferred_deletion=true"
-    ]
-}
+echo -e '{' > /etc/docker/daemon.json
+echo -e '    "storage-driver": "devicemapper",' >> /etc/docker/daemon.json
+echo -e '    "storage-opts": [' >> /etc/docker/daemon.json
+echo -e '    "dm.thinpooldev=/dev/mapper/docker-thinpool",' >> /etc/docker/daemon.json
+echo -e '    "dm.use_deferred_removal=true",'>> /etc/docker/daemon.json
+echo -e '    "dm.use_deferred_deletion=true"' >> /etc/docker/daemon.json
+echo -e '    ]' >> /etc/docker/daemon.json
+echo -e '}' >> /etc/docker/daemon.json
+
+
+service docker restart
+

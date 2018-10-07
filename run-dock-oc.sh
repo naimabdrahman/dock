86be6078dd571751bad49773eb0920e8645767cf
naimabdrahman <naim.abdrahman@gmail.com>
2018-10-07 20:18:18 +0800

    Update run-dock-oc.sh

--- run-dock-oc.sh
+++ run-dock-oc.sh
@@ -1,1 +1,8 @@
-docker run -d -p 8080:80 -v /data/owncloud/:/var/www/html owncloud
+## run owncloud with persistent volume
+# in container : rsync -ahP /var/www/html into /temp (mounted)
+# in container : set permission in /semp similar to /var/www/html
+docker run -dit -p 80:80 -v /mnt/data/oc/html:/var/www/html --restart unless-stopped --name owncloud owncloud
+
+
+## run owncloud without persistent volume
+#docker run -dit -p 80:80 --name owncloud owncloud

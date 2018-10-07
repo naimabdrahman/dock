39720e6106fea26d44b453fe02d643ef1c8126ad
naimabdrahman <naim.abdrahman@gmail.com>
2018-10-07 21:19:22 +0800

    Update run-dock-stack.sh

--- run-dock-stack.sh
+++ run-dock-stack.sh
@@ -1,4 +1,4 @@
 ## Deploy docker stack
 
 docker stack deploy -c docker-compose.yml stack1
-#docker stack deploy --compose-file docker-compose.yml stackdemo:
+#docker stack deploy --compose-file docker-compose.yml stackdemo

0c468e7ab7fb311ac8ced16397e31ba9d1aa1e9b
naimabdrahman <naim.abdrahman@gmail.com>
2019-04-24 21:58:14 +0800

    Update run-dock-elk.sh

--- elk/run-dock-elk.sh
+++ elk/run-dock-elk.sh
@@ -1,6 +1,10 @@
 ## script to run elk in docker mode
 ## prerequisite: elk in docker mode should've been installed first and memory should've been configured
 
+# adjust vm.max_map_count=262144 to start elastic search (tested with 4GB ram VM)
+sysctl -w vm.max_map_count=262144
+
+
 docker rm -f elk
 docker run -p 5601:5601 -p 9200:9200 -p 5044:5044 -it --name elk sebp/elk
 

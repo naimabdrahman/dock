ec8984931d3d1a6f88aa4b2334b35f9c38d77a57
naimabdrahman <naim.abdrahman@gmail.com>
2019-07-07 10:46:17 +0800

    Sun Jul  7 10:45:47 +08 2019

---
+++ gitlab/run-dock-gitlab-ce.sh
@@ -0,0 +1,11 @@
+docker run --detach \
+  --hostname gitlab.nar.test \
+  --publish 443:443 --publish 80:80 --publish 1022:22 \
+  --name gitlab \
+  --restart always \
+  --volume /srv/gitlab/config:/etc/gitlab \
+  --volume /srv/gitlab/logs:/var/log/gitlab \
+  --volume /srv/gitlab/data:/var/opt/gitlab \
+  gitlab/gitlab-ce:latest
+
+# tested with 2cpu and 4GB ram

0380d2fce2fba506e9cd48d1feebfb3e24f3ef0d
naimabdrahman <naim.abdrahman@gmail.com>
2019-11-03 12:20:37 +0800

    Update run-dock-postgres.sh

--- run-dock-postgres.sh
+++ run-dock-postgres.sh
@@ -1,3 +1,3 @@
 ## run postgres docker on local
 ## similar to installing/running postgres locally
-docker run --net=host -it --rm -e POSTGRES_PASSWORD=password postgres
+docker run --net=host -dit --rm -e POSTGRES_PASSWORD=password postgres

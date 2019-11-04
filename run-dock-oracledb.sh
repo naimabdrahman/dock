5b8ffc255c424f82e3e4bc9830807c4d0b4dcab1
naimabdrahman <naim.abdrahman@gmail.com>
2019-11-04 12:29:21 +0800

    Create run-dock-oracledb.sh

---
+++ run-dock-oracledb.sh
@@ -0,0 +1,6 @@
+## run oracledb for testing purpose
+## reference : https://hub.docker.com/r/oracleinanutshell/oracle-xe-11g
+
+
+docker run --net=host -dit --rm -e ORACLE_ALLOW_REMOTE=true oracleinanutshell/oracle-xe-11g
+

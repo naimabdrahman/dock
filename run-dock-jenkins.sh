f907323e13236e3913e8e23096aea6c447767061
naimabdrahman <naim.abdrahman@gmail.com>
2019-11-16 00:08:53 +0000

    update image

--- run-dock-jenkins.sh
+++ run-dock-jenkins.sh
@@ -1,1 +1,2 @@
-docker run -d -p 8080:8080 -p 50000:50000 -v /home/jenkins:/var/jenkins_home jenkins
+#docker run -dit --rm --name jenkins -p 8080:8080 -p 50000:50000 -v /home/jenkins:/var/jenkins_home jenkins/jenkins
+docker run -dit --rm --name jenkins --net=host -v /data/jenkins:/var/jenkins_home jenkins/jenkins

b8320fdb1b0efd9b5868d941bb321058dd8e0399
naimabdrahman <naim.abdrahman@gmail.com>
2019-11-16 00:19:37 +0000

    Sat Nov 16 00:19:37 UTC 2019

--- run-dock-jfrog.sh
+++ run-dock-jfrog.sh
@@ -1,6 +1,5 @@
 ## docker image for jfrog
 ## reference : https://www.jfrog.com/confluence/display/RTF/Installing+with+Docker
 
-
-
-docker run --name artifactory -d -p 8081:8081 docker.bintray.io/jfrog/artifactory-cpp-ce:latest
+#docker run --name artifactory -d -p 8081:8081 docker.bintray.io/jfrog/artifactory-cpp-ce:latest
+docker run -dit --rm --name=jfrog --net=host docker.bintray.io/jfrog/artifactory-cpp-ce:latest

ebd7450f8ff5847e739977da18bb2ab49242b8b9
naimabdrahman <naim.abdrahman@gmail.com>
2020-05-01 09:08:56 +0800

    Update init-swarm.sh

--- init-swarm.sh
+++ init-swarm.sh
@@ -1,3 +1,3 @@
 # to start init swarm mode on centos 7
 docker swarm init  > /root/swarm-info.txt
-cat /root/swarm-info.txt | sed -n 5p
+cat /root/swarm-info.txt | sed -n 5p > /root/swarm-info-2.txt 

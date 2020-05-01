ea8005d55de2f070315f5e82cd4b88faded3b664
naimabdrahman <naim.abdrahman@gmail.com>
2020-05-01 11:27:03 +0800

    Update run-dock-visual-gui.sh

--- run-dock-visual-gui.sh
+++ run-dock-visual-gui.sh
@@ -1,1 +1,3 @@
+## reference : https://awesomeopensource.com/project/JulienBreux/docker-swarm-gui
+
 docker run -it --rm -p 8080:8080 -v /var/run/docker.sock:/var/run/docker.sock  julienbreux/docker-swarm-gui:latest

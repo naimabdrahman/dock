640611e56e19dc6839e682744ddd56953465f574
naimabdrahman <naim.abdrahman@gmail.com>
2020-05-03 04:06:42 +0000

    run-dock-nfs.sh

--- volume/run-dock-nfs.sh
+++ volume/run-dock-nfs.sh
@@ -4,3 +4,12 @@ docker run --rm -itd --name nfs \
   -e SHARED_DIRECTORY=/nfs.1 \
   -p 2049:2049 \
   itsthenetwork/nfs-server-alpine:latest
+
+echo -e "
+
+run below to test mount (create dir /mnt/temp if not available);  
+
+mount -t nfs4 127.0.0.1:/ /mnt/temp
+
+
+"

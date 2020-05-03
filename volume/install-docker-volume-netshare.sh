b45430b5f6b6c2aa1471b5d64a2cca4be51e8859
naimabdrahman <naim.abdrahman@gmail.com>
2020-05-03 10:56:21 +0800

    Create install-docker-volume-netshare.sh

---
+++ volume/install-docker-volume-netshare.sh
@@ -0,0 +1,42 @@
+## install docker-volume-netshare on docker host
+## note : to use nfs on docker swarm
+## reference : https://sysadmins.co.za/docker-swarm-persistent-storage-with-nfs/
+
+
+
+
+## install nfs-utils
+yum install -y nfs-utils 
+
+
+## install golang
+curl https://raw.githubusercontent.com/naimabdrahman/shell/master/install-golang.sh | bash
+
+
+## install docker-volume-netshare
+cd ; go get github.com/ContainX/docker-volume-netshare
+cd /root/go/src/github.com/ContainX/docker-volume-netshare
+go build
+scp -rp docker-volume-netshare /usr/local/bin/
+docker-volume-netshare nfs -a 1.40
+
+
+## create docker-volume-netshare service
+touch /etc/systemd/system/docker-volume-netshare.service
+cat > /etc/systemd/system/docker-volume-netshare.service <<EOL
+[Unit]
+Description=docker-volume-netshare
+
+[Service]
+User=root
+ExecStart=/usr/local/bin/docker-volume-netshare nfs -a 1.40
+
+[Install]
+WantedBy=default.target
+EOL
+
+
+## enable service and run permanently
+systemctl daemon-reload
+systemctl enable docker-volume-netshare.service
+systemctl start docker-volume-netshare.service

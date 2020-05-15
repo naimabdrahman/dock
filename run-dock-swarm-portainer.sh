4e49949a90b0baa0c6def670b9ad1a6fdaf74b2f
naimabdrahman <naim.abdrahman@gmail.com>
2020-05-15 04:27:21 +0000

    run-dock-swarm-portainer.sh

--- run-dock-swarm-portainer.sh
+++ run-dock-swarm-portainer.sh
@@ -3,6 +3,6 @@ docker service create \
     --publish 9000:9000 \
     --constraint 'node.role == manager' \
     --mount type=bind,src=/host/data,dst=/data \
-     --mount type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock \
+    --mount type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock \
     portainer/portainer \
     -H unix:///var/run/docker.sock

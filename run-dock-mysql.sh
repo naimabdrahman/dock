b3aa499d89ad80813e58a63e0f35d37cc05b64a6
naimabdrahman <naim.abdrahman@gmail.com>
2020-06-06 23:27:56 +0800

    Update run-dock-mysql.sh

--- run-dock-mysql.sh
+++ run-dock-mysql.sh
@@ -1,7 +1,17 @@
 docker run \
 --detach \
 --name=mysql-server \
---env="MYSQL_ROOT_PASSWORD=mypassword" \
+--env="MYSQL_ROOT_PASSWORD=password" \
 --publish 6603:3306 \
 --volume=/data/mysql/dir:/var/lib/mysql \
 mysql
+
+
+echo -e "
+just in case id there is issue with encryption and access
+
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'password';  flush privileges;
+
+ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'password';
+#reference https://stackoverflow.com/questions/49194719/authentication-plugin-caching-sha2-password-cannot-be-loaded
+"

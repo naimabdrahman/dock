6d075725c8c7a349367ca82370abe0e2e677c8d8
naimabdrahman <naim.abdrahman@gmail.com>
2020-06-16 07:12:51 +0000

    run-dock-elk.sh

--- elk/run-dock-elk.sh
+++ elk/run-dock-elk.sh
@@ -6,6 +6,6 @@ sysctl -w vm.max_map_count=262144
 
 
 docker rm -f elk
-docker run -p 5601:5601 -p 9200:9200 -p 5044:5044 -it --name elk sebp/elk
+docker run -p 5601:5601 -p 9200:9200 -p 5044:5044 -dit --rm --name elk sebp/elk 
 
 

971d9a906b97ae35ea926a1fe611db063f9182ba
naimabdrahman <naim.abdrahman@gmail.com>
2021-02-23 16:45:51 +0800

    Update run-dock-rancher.sh

--- run-dock-rancher.sh
+++ run-dock-rancher.sh
@@ -1,2 +1,7 @@
 ## run on 4cpu 8gb ram
-docker run -dit --privileged --restart=unless-stopped -p 80:80 -p 443:443 rancher/rancher
+
+# no persistent volume
+#docker run -dit --privileged --restart=unless-stopped -p 80:80 -p 443:443 rancher/rancher
+
+# with persistent volume
+docker run -dit --privileged --restart=unless-stopped --name rancher -p 80:80 -p 443:443 -v ranch:/var/lib/rancher rancher/rancher

7274d54e4c918493555eba2cc60e62da93ca101f
naimabdrahman <naim.abdrahman@gmail.com>
2021-02-24 12:23:34 +0800

    Update install-dock-ce.sh

--- install-dock-ce.sh
+++ install-dock-ce.sh
@@ -3,7 +3,7 @@
 yum -y install epel-release
 #yum -y update
 yum -y remove docker docker-common docker-selinux docker-engine
-yum install -y yum-utils device-mapper-persistent-data lvm2
+yum install -y yum-utils device-mapper-persistent-datahttps://github.com/naimabdrahman/dock/stargazers lvm2
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 #yum makecache fast
 yum -y install docker-ce
@@ -15,7 +15,7 @@ chkconfig docker on
 #yum -y install python-pip
 yum -y install python3-pip
 # Upgrade pip
-pip3 install --upgrade pip3
+pip3 install --upgrade pip3 ; pip3 install --upgrade pip
 # compose
 #pip install docker-compose
 pip3 install docker-compose

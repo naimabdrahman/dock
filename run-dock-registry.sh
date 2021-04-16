5fd8c2ae8ab5bb0e1ece796f9cd3ddcdc635741c
root <root@jb.nar>
2021-04-16 12:50:58 +0800

    run-dock-registry.sh

--- run-dock-registry.sh
+++ run-dock-registry.sh
@@ -3,8 +3,8 @@
 
 
 docker run -dit \
-  --restart=always \
-  --rm --name registry \
+  --rm \
+  --name registry \
   -v /etc/pki/tls/certs:/certs \
   -e REGISTRY_HTTP_ADDR=0.0.0.0:443 \
   -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/dock.crt \

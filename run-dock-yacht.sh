d0c70036404c81528b4f97ab65f3ec82379eca49
naimabdrahman <naim.abdrahman@gmail.com>
2021-06-27 22:02:16 +0800

    Create run-dock-yacht.sh

---
+++ run-dock-yacht.sh
@@ -0,0 +1,6 @@
+## https://yacht.sh/docs/Installation/Getting_Started
+
+docker run -dit -p 8000:8000 -v /var/run/docker.sock:/var/run/docker.sock -v yacht:/config selfhostedpro/yacht
+
+# u admin@yacht.local
+# p pass 

1e4e86ca38caacd64de6c0988a56cc9a98f48dd1
naimabdrahman <naim.abdrahman@gmail.com>
2021-07-22 15:33:47 +0000

    Thu Jul 22 15:33:47 UTC 2021

--- run-dock-yacht.sh
+++ run-dock-yacht.sh
@@ -1,4 +1,5 @@
 ## https://yacht.sh/docs/Installation/Getting_Started
+# alternative to portainer
 
 docker run -dit -p 8000:8000 -v /var/run/docker.sock:/var/run/docker.sock -v yacht:/config selfhostedpro/yacht
 

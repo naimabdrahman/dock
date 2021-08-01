b98d30989907a52bbcf13d432ef39df2e3e5c3d8
naimabdrahman <naim.abdrahman@gmail.com>
2021-08-01 13:47:40 +0000

    gitlab

--- run-dock-gitlab.sh
+++ run-dock-gitlab.sh
@@ -1,3 +1,6 @@
+## run docker gitlab
+## ref ==> https://stackoverflow.com/questions/60062065/gitlab-initial-root-password
+
 docker run --detach \
     --hostname gitlab.example.com \
     --publish 443:443 --publish 80:80 --publish 8022:22 \
@@ -6,4 +9,6 @@ docker run --detach \
     --volume /srv/gitlab/config:/etc/gitlab \
     --volume /srv/gitlab/logs:/var/log/gitlab \
     --volume /srv/gitlab/data:/var/opt/gitlab \
-    gitlab/gitlab-ce:latest
+    -e GITLAB_ROOT_EMAIL="root@local" \
+    -e GITLAB_ROOT_PASSWORD="gitlab_root_password" \
+    gitlab/gitlab-ee:latest

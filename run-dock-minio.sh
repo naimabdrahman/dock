b4b37f50fb8388cc2dbcdac3fae04bfc48afd925
naimabdrahman <naim.abdrahman@gmail.com>
2021-09-03 02:00:32 +0000

    run-dock-minio.sh

--- run-dock-minio.sh
+++ run-dock-minio.sh
@@ -1,6 +1,22 @@
 ## quick run minio docker
 # reference : https://hub.docker.com/r/minio/minio/
+# https://docs.min.io/docs/minio-docker-quickstart-guide.html
 
+
+'''
+# quick run
 docker run -it -p 9000:9000 minio/minio server /data
+'''
+
+# detail run
+docker run -dit \
+  -p 9000:9000 \
+  -p 9001:9001 \
+  -v minio:/data \
+  -e "MINIO_ROOT_USER=rootuser" \
+  -e "MINIO_ROOT_PASSWORD=rootpassword" \
+  --rm \
+  --name minio \
+  quay.io/minio/minio server /data --console-address ":9001"
 
 

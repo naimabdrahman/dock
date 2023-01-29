f743305aa5a9d6b21d1349d0d660aa1890097fb7
naimabdrahman <naim.abdrahman@gmail.com>
2023-01-29 21:59:51 +0800

    Update run-dock-shellinabox.sh

--- run-dock-shellinabox.sh
+++ run-dock-shellinabox.sh
@@ -7,3 +7,18 @@ docker container run -dit \
         -e SIAB_SUDO=true \
         -v $PWD/dev:/usr/src/dev \
         sspreitzer/shellinabox:latest
+
+
+'''
+docker container run -dit \
+        --name shellinabox \
+        --restart always \
+        -p 80:4200 \
+        -e SIAB_USER=chillexbro \
+        -e SIAB_PASSWORD=Kasihantam \
+        -e SIAB_SUDO=true \
+        -e SIAB_SSL=false \
+        -v $PWD/dev:/usr/src/dev \
+        sspreitzer/shellinabox:latest
+
+'''

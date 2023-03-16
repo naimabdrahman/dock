98e6e7cce0e606450bce2ad1a860f58fd220823e
naimabdrahman <naim.abdrahman@gmail.com>
2023-03-16 15:35:40 +0800

    Update run-dock-mysql.sh

--- run-dock-mysql.sh
+++ run-dock-mysql.sh
@@ -11,6 +11,7 @@ echo -e "
 just in case id there is issue with encryption and access
 
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'password';  flush privileges;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';  flush privileges;
 
 ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'password';
 #reference https://stackoverflow.com/questions/49194719/authentication-plugin-caching-sha2-password-cannot-be-loaded

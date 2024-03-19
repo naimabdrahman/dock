4df94571fd8ba8d99a2e5e8e158077417dc99ebb
naimabdrahman <naim.abdrahman@gmail.com>
2024-03-19 10:05:13 +0000

    Tue Mar 19 10:05:13 UTC 2024

--- run-dock-mysql.sh
+++ run-dock-mysql.sh
@@ -13,8 +13,8 @@ just in case id there is issue with encryption and access
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'password';  flush privileges;
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';  flush privileges;
 
-CREATE USER 'dba'@'%' IDENTIFIED BY 'password' WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON *.* TO 'dba'@'%' IDENTIFIED BY 'password';  flush privileges;
+CREATE USER 'dba'@'%' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON *.* TO 'dba'@'%';  flush privileges;
 
 ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'password';
 #reference https://stackoverflow.com/questions/49194719/authentication-plugin-caching-sha2-password-cannot-be-loaded

f5232faaf319f94b331f62a7973b63efbe6fa01d
naimabdrahman <naim.abdrahman@gmail.com>
2026-02-17 11:34:49 +0000

    ssl cert tested on Tue Feb 17 11:34:49 AM UTC 2026

---
+++ compose/traefik/ssl/init-letsencrypt.sh
@@ -0,0 +1,3 @@
+mkdir -p /root/git/dock/compose/traefik/ssl/letsencrypt
+touch /root/git/dock/compose/traefik/ssl/letsencrypt/acme.json
+chmod 600 /root/git/dock/compose/traefik/ssl/letsencrypt/acme.json

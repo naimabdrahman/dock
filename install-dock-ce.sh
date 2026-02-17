c3b957eeb783ea45356174c81fbd98b7d949263c
naimabdrahman <naim.abdrahman@gmail.com>
2026-02-17 12:35:33 +0800

    Fix curl command for downloading docker-compose

--- install-dock-ce.sh
+++ install-dock-ce.sh
@@ -29,6 +29,6 @@ pip3 install --upgrade pip3 ; pip3 install --upgrade pip
 ## compose - install via curl
 #curl -L https://github.com/docker/compose/releases/download/v2.7.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 #curl -L https://github.com/docker/compose/releases/download/v2.27.1/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-curl -L "https://github.com(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 
 chmod +x /usr/local/bin/docker-compose
